7851e9e41fe591937e65973296f66d6353e5d58d
Yi Fan Jian <yifan9702@gmail.com>
2021-05-02 10:30:05 -0400

    added changes for deployment

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const port = 8080;
+const port = 443;
 
 const options = {
     key: fs.readFileSync(path.join(__dirname, "/certs/key.pem")),
@@ -23,5 +23,5 @@ app.get("/apikey", jwtService.generateToken);
 let server = https.createServer(options, app);
 
 server.listen(port, () => {
-    console.log(`Safe API app listening at https://localhost:${port}`);
+    console.log(`Safe API app listening at port: ${port}`);
 });
